18668aa7124b738cbc2804c1416b6bb444cfc148
Daniel Silva Ferreira <danielsilva@usp.br>
2024-03-11 11:22:06 -0300

    Update anotacoes.php

--- anotacoes.php
+++ anotacoes.php
@@ -1,5 +1,198 @@
 <?php
+//plugins/importexport/ompMarc/ompMarc.inc.php
+import('plugins.importexport.ompMarc.lib.pkp.classes.plugins.ImportExportPlugin');
 
+class ompMarc extends ImportExportPlugin2
+{
+    /**
+     * Constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * @copydoc Plugin::register()
+     */
+    public function register($category, $path, $mainContextId = null)
+    {
+        $success = parent::register($category, $path, $mainContextId);
+        if (!Config::getVar('general', 'installed') || defined('RUNNING_UPGRADE')) {
+            return $success;
+        }
+        if ($success && $this->getEnabled()) {
+            $this->addLocaleData();
+            $this->import('ompMarcDeployment');
+        }
+
+        return $success;
+    }
+
+    public function display($args, $request)
+    {
+        $templateMgr = TemplateManager::getManager($request);
+        $context = $request->getContext();
+
+        parent::display($args, $request);
+
+        $templateMgr->assign('plugin', $this);
+
+        switch (array_shift($args)) {
+            //aqui monta a página do plugin
+            case 'index':
+            case '':
+                $apiUrl = $request->getDispatcher()->url($request, ROUTE_API, $context->getPath(), 'submissions');
+                $submissionsListPanel = new \APP\components\listPanels\SubmissionsListPanel(
+                    'submissions',
+                    __('common.publications'),
+                    [
+                        'apiUrl' => $apiUrl,
+                        'count' => 100,
+                        'getParams' => [
+                            'status' => [STATUS_PUBLISHED], // Filtro para livros publicados
+                        ],
+                        'lazyLoad' => true,
+                    ]
+                );
+        
+                $submissionsConfig = $submissionsListPanel->getConfig();
+                $submissionsConfig['addUrl'] = '';
+                $submissionsConfig['filters'] = array_slice($submissionsConfig['filters'], 1);
+        
+                $templateMgr->setState([
+                    'components' => [
+                        'submissions' => $submissionsConfig,
+                    ],
+                ]);
+        
+                $templateMgr->assign([
+                    'pageComponent' => 'ImportExportPage',
+                ]);
+        
+                $templateMgr->display($this->getTemplateResource('index.tpl'));
+                break;
+                //aqui exporta o livro
+                case 'export':
+                    $exportXml = $this->exportSubmissions(
+                        (array) $request->getUserVar('selectedSubmissions'),
+                        $request->getContext(),
+                        $request->getUser(),
+                        $request
+                    );
+                    import('lib.pkp.classes.file.FileManager');
+                    $fileManager = new FileManager();
+                    //nome do arquivo e formato txt - trocar por rec
+                    $exportFileName = $this->getExportPath() . '/omp.mrc';
+                    $fileManager->writeFile($exportFileName, $exportXml);
+                    $fileManager->downloadByPath($exportFileName);
+                    $fileManager->deleteByPath($exportFileName);
+                    break;
+            default:
+                $dispatcher = $request->getDispatcher();
+                $dispatcher->handle404();
+        }
+    }
+
+    // forma o link de acesso a ferramenta
+    public function getName()
+    {
+        return 'ompMarc';
+    }
+
+    /**
+     * Get the display name.
+     *
+     * @return string
+     */
+    public function getDisplayName()
+    {
+        return __('plugins.importexport.ompMarc.displayName');
+    }
+
+    /**
+     * Get the display description.
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return __('plugins.importexport.ompMarc.description');
+    }
+
+    //forma o prefixo do arquivo .xml -desnecessário
+    public function getPluginSettingsPrefix()
+    {
+        return 'ompMarc';
+    }
+    
+    //função que obtém as cidades com base no copyrightholder
+    function obterCidade($copyright) {
+        $mapeamentoCidades = [
+            'São Paulo' => [
+                'Escola de Artes, Ciências e Humanidades', 'Escola de Artes, Ciências e Humanidades ', 'Escola de Comunicações e Artes', 'Escola de Comunicações e Artes ', 
+                'Escola de Educação Física e Esporte', 'Escola de Educação Física e Esporte ', 'Escola de Enfermagem', 'Escola de Enfermagem ', 
+                'Escola Politécnica', 'Escola Politécnica ', 'Faculdade de Arquitetura e Urbanismo', 'Faculdade de Arquitetura e Urbanismo ', 
+                'Faculdade de Ciências Farmacêuticas', 'Faculdade de Ciências Farmacêuticas ', 'Faculdade de Direito', 'Faculdade de Direito ', 
+                'Faculdade de Economia, Administração e Contabilidade', 'Faculdade de Economia, Administração e Contabilidade ', 
+                'Faculdade de Educação', 'Faculdade de Educação ', 'Faculdade de Filosofia, Letras e Ciências Humanas', 'Faculdade de Filosofia, Letras e Ciências Humanas ', 
+                'Faculdade de Medicina', 'Faculdade de Medicina ', 'Faculdade de Medicina Veterinária e Zootecnia', 'Faculdade de Medicina Veterinária e Zootecnia ', 
+                'Faculdade de Odontologia', 'Faculdade de Odontologia ', 'Faculdade de Saúde Pública', 'Faculdade de Saúde Pública ', 
+                'Instituto de Astronomia, Geofísica e Ciências Atmosféricas','Instituto de Astronomia, Geofísica e Ciências Atmosféricas ', 
+                'Instituto de Biociências', 'Instituto de Biociências ', 'Instituto de Ciências Biomédicas', 'Instituto de Ciências Biomédicas ', 
+                'Instituto de Energia e Ambiente', 'Instituto de Energia e Ambiente ', 'Instituto de Estudos Avançados', 'Instituto de Estudos Avançados ', 
+                'Instituto de Estudos Brasileiros', 'Instituto de Estudos Brasileiros ','Instituto de Física', 'Instituto de Física ', 
+                'Instituto de Geociências', 'Instituto de Geociências ', 'Instituto de Matemática e Estatística', 'Instituto de Matemática e Estatística ', 
+                'Instituto de Medicina Tropical de São Paulo', 'Instituto de Medicina Tropical de São Paulo ', 
+                'Instituto de Psicologia', 'Instituto de Psicologia ', 'Instituto de Química', 'Instituto de Química ', 
+                'Instituto de Relações Internacionais', 'Instituto de Relações Internacionais ', 
+                'Instituto Oceanográfico', 'Instituto Oceanográfico ', 'Museu de Arqueologia e Etnografia', 'Museu de Arqueologia e Etnografia ', 
+                'Museu de Arte Contemporânea', 'Museu de Arte Contemporânea ', 'Museu Paulista', 'Museu Paulista ', 
+                'Museu de Zoologia', 'Museu de Zoologia ', 
+            ],
+
+            'Bauru' => [
+                'Faculdade de Odontologia de Bauru', 'Faculdade de Odontologia de Bauru ', 'Hospital de Reabilitação de Anomalias Craniofaciais', 'Hospital de Reabilitação de Anomalias Craniofaciais ',
+            ],
+
+            'Lorena' => [
+                'Escola de Engenharia de Lorena', 'Escola de Engenharia de Lorena ', 
+            ],
+
+            'Piracicaba' => [
+                'Centro de Energia Nuclear na Agricultura', 'Centro de Energia Nuclear na Agricultura ', 'Escola Superior de Agricultura “Luiz de Queiroz”', 'Escola Superior de Agricultura “Luiz de Queiroz” ', 'Escola Superior de Agricultura Luiz de Queiroz', 'Escola Superior de Agricultura Luiz de Queiroz ', 
+            ],
+            
+            'Pirassununga' => [
+                'Faculdade de Zootecnia e Engenharia de Alimentos', 'Faculdade de Zootecnia e Engenharia de Alimentos ',
+            ],
+            
+            'Ribeirão Preto' => [
+                'Escola de Educação Física e Esporte de Ribeirão Preto', 'Escola de Educação Física e Esporte de Ribeirão Preto ', 'Escola de Enfermagem de Ribeirão Preto', 'Escola de Enfermagem de Ribeirão Preto ', 'Faculdade de Ciências Farmacêuticas de Ribeirão Preto', 'Faculdade de Ciências Farmacêuticas de Ribeirão Preto ', 'Faculdade de Direito de Ribeirão Preto', 'Faculdade de Direito de Ribeirão Preto ', 'Faculdade de Economia, Administração e Contabilidade de Ribeirão Preto', 'Faculdade de Economia, Administração e Contabilidade de Ribeirão Preto ', 'Faculdade de Filosofia, Ciências e Letras de Ribeirão Preto', 'Faculdade de Filosofia, Ciências e Letras de Ribeirão Preto ', 'Faculdade de Medicina de Ribeirão Preto', 'Faculdade de Medicina de Ribeirão Preto ', 'Faculdade de Odontologia de Ribeirão Preto', 'Faculdade de Odontologia de Ribeirão Preto ', 
+            ],
+
+            'Santos' => [
+                'Departamento de Engenharia de Minas e Petróleo', 'Departamento de Engenharia de Minas e Petróleo ',
+            ],
+
+            'São Carlos' => [
+                'Escola de Engenharia de São Carlos', 'Escola de Engenharia de São Carlos ', 'Instituto de Arquitetura e Urbanismo', 'Instituto de Arquitetura e Urbanismo ', 'Instituto de Ciências Matemáticas e de Computação', 'Instituto de Ciências Matemáticas e de Computação ', 'Instituto de Física de São Carlos', 'Instituto de Física de São Carlos ', 'Instituto de Química de São Carlos', 'Instituto de Química de São Carlos ', 
+            ],
+
+        ];
+    
+        $copyright = str_replace('Universidade de São Paulo. ', '', $copyright);
+        foreach ($mapeamentoCidades as $cidade => $variacoes) {
+            if (in_array($copyright, $variacoes)) {
+                return $cidade;
+            }
+        }
+    
+        // Se não houver correspondência, retorna 'LOCAL'
+        return 'LOCAL';
+    }
+    
+    
     /**
      * FUNÇÃO PRINCIPAL, RESPOSÁVEL PELA ESTRUTURA DO ARQUIVO mrk.
      */
@@ -129,12 +322,10 @@ if (strpos($copyright, 'Universidade de São Paulo. ') === 0) {
 $currentDateTime = date('d.m.Y');
     $cincoZeroZero=  'aDisponível em: '.htmlspecialchars($publicationUrl) . '. Acesso em: '.$currentDateTime;
     
-   // Demais autoras - Sobrenome, Nome - Orcid - Afiliação - País
-$additionalAuthors = array_slice($authorsInfo, 1); // Pular o primeiro autor
+    // Demais autoras - Sobrenome, Nome - Orcid - Afiliação - País
+    $additionalAuthors = array_slice($authorsInfo, 1); // Pular o primeiro autor
 
-$seteZeroZero = ''; // Inicializa a string vazia
-
-foreach ($additionalAuthors as $additionalAuthor) {
+    foreach ($additionalAuthors as $additionalAuthor) {
     $additionalAuthorInfo = [
         'givenName' => $additionalAuthor['givenName'],
         'surname' => $additionalAuthor['surname'],
@@ -143,25 +334,19 @@ foreach ($additionalAuthors as $additionalAuthor) {
         'locale' => $additionalAuthor['locale'],
     ];
 
-    $authorString = '';
-
     if (!empty($additionalAuthorInfo['orcid']) && !empty($additionalAuthorInfo['afiliation'])) {
-        $authorString = '1 a' . htmlspecialchars($additionalAuthorInfo['surname']) . ', ' . htmlspecialchars($additionalAuthorInfo['givenName']) . '0' . $additionalAuthorInfo['orcid'] . '4colab5(*)7INT8' . htmlspecialchars($additionalAuthorInfo['afiliation']) . '9' . htmlspecialchars($additionalAuthorInfo['locale']);
+        $seteZeroZero = '1 a' . htmlspecialchars($additionalAuthorInfo['surname']) . ', ' . htmlspecialchars($additionalAuthorInfo['givenName']) . '0' . $additionalAuthorInfo['orcid'] . '4colab5(*)7INT8' . htmlspecialchars($additionalAuthorInfo['afiliation']) . '9' . htmlspecialchars($additionalAuthorInfo['locale']);
     } elseif (!empty($additionalAuthorInfo['orcid'])) {
         // Adiciona apenas o ORCID se presente
-        $authorString = '1 a' . htmlspecialchars($additionalAuthorInfo['surname']) . ', ' . htmlspecialchars($additionalAuthorInfo['givenName']) . '0' . $additionalAuthorInfo['orcid'] . '4colab5(*)$7INT9' . htmlspecialchars($additionalAuthorInfo['locale']);
+        $seteZeroZero = '1 a' . htmlspecialchars($additionalAuthorInfo['surname']) . ', ' . htmlspecialchars($additionalAuthorInfo['givenName']) . '0' . $additionalAuthorInfo['orcid'] . '4colab5(*)$7INT9' . htmlspecialchars($additionalAuthorInfo['locale']);
     } elseif (!empty($additionalAuthorInfo['afiliation'])) {
         // Adiciona apenas a afiliação se presente
-        $authorString = '1 a' . htmlspecialchars($additionalAuthorInfo['surname']) . ', ' . htmlspecialchars($additionalAuthorInfo['givenName']) . '4colab5(*)7INT8' . htmlspecialchars($additionalAuthorInfo['afiliation']) . '9' . htmlspecialchars($additionalAuthorInfo['locale']);
+        $seteZeroZero = '1 a' . htmlspecialchars($additionalAuthorInfo['surname']) . ', ' . htmlspecialchars($additionalAuthorInfo['givenName']) . '4colab5(*)7INT8' . htmlspecialchars($additionalAuthorInfo['afiliation']) . '9' . htmlspecialchars($additionalAuthorInfo['locale']);
     } else {
         // Adiciona sem ORCID e afiliação se nenhum estiver presente
-        $authorString = '1 a' . htmlspecialchars($additionalAuthorInfo['surname']) . ', ' . htmlspecialchars($additionalAuthorInfo['givenName']) . '4colab5(*)9' . htmlspecialchars($additionalAuthorInfo['locale']);
+        $seteZeroZero = '1 a' . htmlspecialchars($additionalAuthorInfo['surname']) . ', ' . htmlspecialchars($additionalAuthorInfo['givenName']) . '4colab5(*)9' . htmlspecialchars($additionalAuthorInfo['locale']);
     }
-
-    // Concatena a string do autor adicional à string geral
-    $seteZeroZero .= $authorString;
 }
-
 
     //harcoding's abcd usp
 
@@ -221,12 +406,7 @@ $rec500POS = sprintf('%05d', $rec260CAR + $rec260POS);
 $rec500CAR = sprintf('%04d', strlen($cincoZeroZero) + 3);
 $rec500 = '500' . $rec500CAR . $rec500POS;
 
-
-
-
-///aquieeee
-
-
+//rever este trecho, pois pega somente 1 coautor
 // Quantidade de autores adicionais
 $numAutoresAdicionais = count($additionalAuthors);
 // Criação dos campos 700 para autores adicionais
@@ -275,27 +455,6 @@ if ($numAutoresAdicionais > 0) {
 }
 
 $rec856A = '856' . $rec856ACAR . $rec856APOS;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 $rec856BPOS = sprintf('%05d', $rec856ACAR + $rec856APOS);
@@ -370,5 +529,64 @@ if ($numAutoresAdicionais > 0) {
     *
                             * */
         
-     
+       public function executeCLI($scriptName, &$args)
+    {
+        $opts = $this->parseOpts($args, ['no-embed', 'use-file-urls']);
+        $command = array_shift($args);
+        $marcFile = array_shift($args);
+        $pressPath = array_shift($args);
+
+        AppLocale::requireComponents(LOCALE_COMPONENT_APP_MANAGER, LOCALE_COMPONENT_PKP_MANAGER, LOCALE_COMPONENT_PKP_SUBMISSION);
+        $pressDao = DAORegistry::getDAO('PressDAO');
+        $userDao = DAORegistry::getDAO('UserDAO');
+        $press = $pressDao->getByPath($pressPath);
+
+        if (!$press) {
+            if ($pressPath != '') {
+                echo __('plugins.importexport.common.cliError')."\n";
+                echo __('plugins.importexport.common.error.unknownPress', ['pressPath' => $pressPath])."\n\n";
+            }
+            $this->usage($scriptName);
+
+            return;
+        }
+
+        if ($marcFile && $this->isRelativePath($marcFile)) {
+            $marcFile = PWD.'/'.$marcFile;
+        }
+
+        switch ($command) {
+            case 'export':
+                $outputDir = dirname($marcFile);
+                if (!is_writable($outputDir) || (file_exists($marcFile) && !is_writable($marcFile))) {
+                    echo __('plugins.importexport.common.cliError')."\n";
+                    echo __('plugins.importexport.common.export.error.outputFileNotWritable', ['param' => $marcFile])."\n\n";
+                    $this->usage($scriptName);
+
+                    return;
+                }
+
+                if ($marcFile != '') {
+                    switch (array_shift($args)) {
+                        case 'monograph':
+                        case 'monographs':
+                            $selectedSubmissions = array_slice($args, 1);
+                            $marcContent = $this->exportSubmissions($selectedSubmissions);
+                            file_put_contents($marcFile, $marcContent);
+
+                            return;
+                    }
+                }
+                break;
+        }
+        $this->usage($scriptName);
+    }
+
+    /**
+     * @copydoc ImportExportPlugin::usage
+     */
+    public function usage($scriptName)
+    {
+        fatalError('Not implemented.');
+    }
 }
